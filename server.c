af4f6bd99733b86e7e69aa89004db0de52ddc0a5
rasika-r-k <64706808+rasika-r-k@users.noreply.github.com>
2020-05-02 12:38:11 -0700

    Add files via upload
    Assignment 1

---
+++ server.c
@@ -0,0 +1,99 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 8080 
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    //if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    }
+	
+    //below part processes data from the client,seperating this from the above part by creating child process
+
+    pid_t child_pid;
+    child_pid = fork();
+ 
+    //check if child process created successfully
+    if(child_pid < 0){                              
+
+        printf("fork() system call failed");
+
+	exit(EXIT_FAILURE);
+
+    }
+
+    else if(child_pid == 0){
+
+    //setting uid for child process as nobody user(unprevileged user)
+	
+	setuid(-2);
+
+        if(setuid(getuid()) == -2){
+
+           perror("setuid() failed");
+
+           exit(EXIT_FAILURE);
+
+	}
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n");
+    }
+	
+	else{
+
+    	//returning to parent process
+
+        int returnStatus;
+
+        waitpid(child_pid, &returnStatus, 0);
+
+    }
+ 
+    return 0;  
+} 
